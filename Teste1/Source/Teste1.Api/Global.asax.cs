16aba2a8fbb1afe69b92a2f3ecac88c1e0916122
Cairo Araujo <cairo.s.santos@gmail.com>
2018-01-26 21:27:59 -0200

    Adc novas camadas e dlls

---
+++ Teste1/Source/Teste1.Api/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Teste1.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
