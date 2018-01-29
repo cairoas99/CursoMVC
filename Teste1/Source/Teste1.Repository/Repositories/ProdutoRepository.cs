f90fd25091b6f5f951d60e958c306cb30c3122b9
Cairo Araujo <cairo.s.santos@gmail.com>
2018-01-29 20:50:42 -0200

    Finalizando a camada de repositorio

---
+++ Teste1/Source/Teste1.Repository/Repositories/ProdutoRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Teste1.Domain.Entidades;
+using Teste1.Repository.DataBase;
+
+namespace Teste1.Repository.Repositories
+{
+    public class ProdutoRepository : Execucao
+    {
+        private static readonly Conexao conexao = new Conexao();
+
+        public ProdutoRepository() : base(conexao)
+        {
+        }
+        public IEnumerable<Produto> GetProdutos()
+        {
+            ExecuteProcedure("[dbo].[SP_SelProdutos]");
+
+            var produtos = new List<Produto>();
+
+            using (var reader = ExecuteReader())
+            {
+                while (reader.Read())
+                    produtos.Add(new Produto {
+                        Nome = reader.ReadAsString("Nome")
+                    });
+
+            }
+            return produtos;
+        }
+    }
+}
